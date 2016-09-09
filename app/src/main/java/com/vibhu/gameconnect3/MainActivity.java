017f9d12915912f0b92356c189795b95e532371b
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-09 16:09:27 +0530

    Added Some Animation. Set the layout.

--- app/src/main/java/com/vibhu/gameconnect3/MainActivity.java
+++ app/src/main/java/com/vibhu/gameconnect3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.vibhu.gameconnect3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view)
+    {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationX(-1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
     }
 }
