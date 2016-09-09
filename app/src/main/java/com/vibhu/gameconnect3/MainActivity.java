52100d0e9e178cec23e88f9c3158fab46463f48d
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-09 16:38:19 +0530

    Perfect Layout

--- app/src/main/java/com/vibhu/gameconnect3/MainActivity.java
+++ app/src/main/java/com/vibhu/gameconnect3/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //0 = yellow and 1 = red
+    int activePlayer = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +18,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void dropIn(View view)
     {
+
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationX(-1000f);
+        counter.setTranslationY(-1000f);
 
-        counter.setImageResource(R.drawable.yellow);
+        if(activePlayer == 0) {
 
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+            counter.setImageResource(R.drawable.yellow);
+
+            activePlayer = 1;
+        }
+        else{
+            counter.setImageResource(R.drawable.red);
+
+            activePlayer = 0;
+        }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
     }
 }
